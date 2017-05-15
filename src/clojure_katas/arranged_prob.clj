8628d14d7da6a175ec056f23c6a63708a60dcc63
Andrew Montalto <aamontal@gmail.com>
2017-05-15 11:47:09 -0400

    test

--- src/clojure_katas/arranged_prob.clj
+++ src/clojure_katas/arranged_prob.clj
@@ -2,6 +2,12 @@
   (:require [clojure-katas.core :as core]))
 
 ; Original problem source: http://projecteuler.net/problem=100
+
+(defn solve [p, m, n]
+  (if (= n 0)
+    1.0
+    (* (/ m p) (solve (- p 1) (- m 1) (- n 1) ) )))
+
 (core/defproblem prob
   "p: total number of chips,
    m: total number of blue chips,
@@ -12,4 +18,7 @@
    example:
    If a box contains twenty-one coloured discs, composed of 15 blue discs and 6 red discs,
    and 2 discs were taken at random, it can be seen that the probability of taking 2 blue discs, P(BB) = (15/21)×(14/20) = 1/2."
-  [p, m, n])
+  [p, m, n]
+  (solve p m n)
+
+)
